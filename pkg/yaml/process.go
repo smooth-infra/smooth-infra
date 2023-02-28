7a46592094189bc99fcff234472a0cb806d8284f
Giampaolo Falqui <giampaolofalqui@gmail.com>
2023-02-28 03:06:56 +0100

    Processes replacement tags in tests

--- pkg/yaml/process.go
+++ pkg/yaml/process.go
@@ -1,5 +1,13 @@
 package yaml
 
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/smooth-infra/smooth-infra/pkg/terraform"
+	goyaml "gopkg.in/yaml.v3"
+)
+
 type BaseStructure struct {
 	Version int
 	Input   map[string]Input `yaml:"input"`
@@ -15,4 +23,75 @@ type Test struct {
 	Type    string
 	Params  map[string]interface{}
 	Expects map[string]interface{}
+}
+
+func Process(yaml string) (*BaseStructure, error) {
+	structure := &BaseStructure{}
+
+	err := goyaml.Unmarshal([]byte(yaml), &structure)
+	if err != nil {
+		return nil, err
+	}
+	return structure, nil
+}
+
+func (b *BaseStructure) GetInputName() string {
+	var inputName string
+	for k := range b.Input {
+		inputName = k
+		break
+	}
+	return inputName
+}
+
+func (b *BaseStructure) ProcessTests(inputName string, outputs *terraform.Data) {
+	for testKey, test := range b.Tests {
+		test.Name = replaceStringWithOutputValues(test.Name, inputName, outputs)
+
+		result := make(map[string]interface{})
+		for kParam, vParam := range test.Params {
+			switch v := vParam.(type) {
+			case string:
+				result[kParam] = replaceStringWithOutputValues(v, inputName, outputs)
+			default:
+				result[kParam] = vParam
+			}
+			test.Params = result
+		}
+
+		b.Tests[testKey] = test
+	}
+}
+
+func replaceStringWithOutputValues(str string, inputName string, outputs *terraform.Data) string {
+	re := regexp.MustCompile(fmt.Sprintf(`\${input.%s\.(\w+)}`, inputName))
+
+	matches := re.FindAllStringSubmatch(str, -1)
+
+	params := make(map[string]string)
+	for _, match := range matches {
+		key := match[1] // Extract the captured group
+		params[key] = fmt.Sprint(outputs.Variables[match[1]].Value)
+	}
+
+	replacer := func(match string) string {
+		// Extract the key from the match
+		keyMatches := re.FindStringSubmatch(match)
+		if len(keyMatches) != 2 {
+			// Key not found, return the original string
+			return match
+		}
+		key := keyMatches[1]
+
+		// Look up the replacement value in the map
+		value, ok := params[key]
+		if !ok {
+			// Key not found, return the original string
+			return match
+		}
+
+		return value
+	}
+
+	return re.ReplaceAllStringFunc(str, replacer)
 }
--- pkg/yaml/process_test.go
+++ pkg/yaml/process_test.go
@@ -1,19 +1,64 @@
 package yaml
 
 import (
-	"log"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/smooth-infra/smooth-infra/pkg/terraform"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v3"
+	"github.com/stretchr/testify/require"
 )
 
-var data = `
+func TestIfCanDetermineInputName(t *testing.T) {
+	structure, err := Process(getTestData("output.vars"))
+	require.Nil(t, err)
+	assert.Equal(t, "terraform", structure.GetInputName(), "The input name is not what is expected")
+}
+
+func TestIfCanReplaceTerraformOutputsToTests(t *testing.T) {
+	testData := `
+{
+  "address": {
+      "sensitive": false,
+      "type": "string",
+      "value": "google.com"
+  }
+}
+`
+
+	tempFile, err := os.CreateTemp("", "outputs.json")
+	if err != nil {
+		t.Fatalf("Error creating temporary file: %v", err)
+	}
+	defer os.Remove(tempFile.Name())
+	_, err = tempFile.WriteString(testData)
+	if err != nil {
+		t.Fatalf("Error writing test data to file: %v", err)
+	}
+	tempFile.Close()
+
+	yamlTestFile := getTestData(tempFile.Name())
+
+	outputValues, err := terraform.GetOutputValues(tempFile.Name())
+	require.Nil(t, err)
+
+	config, err := Process(yamlTestFile)
+	require.Nil(t, err)
+
+	config.ProcessTests(config.GetInputName(), outputValues)
+
+	assert.Equal(t, "Verify that requesting google.com is giving a 200 OK", config.Tests[0].Name, "The name does not match the expected value")
+	assert.Equal(t, "google.com", config.Tests[0].Params["address"], "The address does not match the expected value")
+}
+
+func getTestData(outputsFilePath string) string {
+	return fmt.Sprintf(`
 ---
 version: 1
 input:
   terraform:
-    outputs_file: output.vars
+    outputs_file: %s
 tests:
   - name: Verify that requesting ${input.terraform.address} is giving a 200 OK
     type: http/request
@@ -22,15 +67,12 @@ tests:
       secure: true
     expects:
       status_code: 200
-`
+    `, outputsFilePath,
+	)
+}
 
 func TestProcessSimpleYaml(t *testing.T) {
-	structure := BaseStructure{}
-
-	err := yaml.Unmarshal([]byte(data), &structure)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
+	structure, err := Process(getTestData("output.vars"))
+	require.Nil(t, err)
 	assert.Equal(t, "output.vars", structure.Input["terraform"].OutputsFile, "The outputs_file is not what is expected")
 }
